6cf1d9b322c50c8a8e3f510ddcb6dd45f7e07ee2
Anirudh Rao B <anirudhraob67@gmail.com>
2026-04-30 21:37:57 +0530

    feat(auth): add authentication module and update backend configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -67,6 +67,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
+    "corsheaders",
+    "authentication",
 ]
 
 MIDDLEWARE = [
